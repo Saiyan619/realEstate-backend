dbb215b831cfac73a87fa698c7099ca32076f66c
olaniyi <arokoyueb11@gmail.com>
2025-01-19 09:12:37 +0100

    setting up backend

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+require('dotenv').config()
+const { default: mongoose } = require('mongoose');
+var cors = require('cors')
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+
+app.use(express.json(),cors({ origin: '*' }));
+const uri = process.env.MONGODB_URL; 
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverSelectionTimeoutMS: 30000, })
+ const db=mongoose.connection;
+
+db.once('open', () => console.log("Connected"))
+
+app.get('/', (req, res) => {
+  res.send('Hello, this is your Express app!, btw your server running on perfectly');
+});
+
+const userRoute = require('./routes/UserRoute') 
+app.use('/user', userRoute, cors())
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
